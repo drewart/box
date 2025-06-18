a3c02dac3e8dd58082eba189a7e7a6eafba76155
Drew Pierce <drpierce@starbucks.com>
2025-06-17 19:38:38 -0700

    fix json out

--- cli/get.go
+++ cli/get.go
@@ -3,22 +3,22 @@ package cli
 import (
 	"fmt"
 	"os"
+
+	"github.com/drewart/box/internal/config"
 	"github.com/spf13/cobra"
 	"github.com/zalando/go-keyring"
-	"github.com/drewart/box/internal/config"
 )
-
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-}	
+}
 
 var getCmd = &cobra.Command{
 	Use:   "get [service] [user]",
 	Short: "Get a password for a service and user",
-	Long: "Get a password for a service and user using the system keyring",
+	Long:  "Get a password for a service and user using the system keyring",
 	Args:  cobra.ExactArgs(2),
-	Run: func(cmd *cobra.Command, args []string) {	
+	Run: func(cmd *cobra.Command, args []string) {
 		service := args[0]
 		user := args[1]
 
@@ -33,10 +33,10 @@ var getCmd = &cobra.Command{
 			fmt.Fprintf(os.Stderr, "Error getting password: %s\n", err)
 			os.Exit(1)
 		}
-		if appUser == nil  {
-			fmt.Fprintf(os.Stderr, "No app user found for service '%s' and user '%s'\n", service, user)	
+		if appUser == nil {
+			fmt.Fprintf(os.Stderr, "No app user found for service '%s' and user '%s'\n", service, user)
 			os.Exit(1)
 		}
-		fmt.Printf(`{"app":"%s", "user":"%s" "password":"%s", "created":"%s"}`, service, user, secret, appUser.CreaatedAt)
+		fmt.Printf(`{"app":"%s", "user":"%s", "password":"%s", "created":"%s"}`, service, user, secret, appUser.CreaatedAt)
 	},
 }
